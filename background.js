88833e62abb18f488f92e64634708da18a038631
andy <ali@junglescout.com>
2021-12-20 18:25:53 +0800

    add blocked site

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
-let color = "#3aa757"
+// Initial default values
+let color = '#3aa757'
 
-chrome.runtime.onInstalled.addListener(()=>{
-  chrome.storage.sync.set({color})
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+chrome.runtime.onInstalled.addListener(async () => {
+  chrome.storage.sync.set({ color })
+  console.log('Default background color set to %cgreen', `color: ${color}`)
+})
---
+++ blockSites.js
@@ -0,0 +1,1 @@
+document.body.innerHTML = `<h1>Get back to work please</h1>`
