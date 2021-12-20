3e49050b2aa1579ad1a8e40cdb0e7a156abcdc12
andy <ali@junglescout.com>
2021-12-20 14:39:59 +0800

    add options page

---
+++ options.js
@@ -0,0 +1,48 @@
+let page = document.getElementById('optionsbuttonDiv')
+
+let selectedClassName = 'current'
+
+const presetButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1']
+
+const handleButtonClick = (event) => {
+  // Remove styling from the previously selected color
+  let current = event.target.parentElement.querySelector(`.${selectedClassName}`)
+  console.log('below current')
+  console.log(current)
+  if (current && current !== event.target) {
+    console.log('in if')
+    console.log(event.target)
+    current.classList.remove(selectedClassName)
+  }
+
+  // Mark the button as selected
+  let color = event.target.dataset.color
+  event.target.classList.add(selectedClassName)
+  chrome.storage.sync.set({ color })
+}
+
+// Add a button to the page for each supplied color
+const constructOptions = (buttonColors) => {
+  chrome.storage.sync.get('color', (colorArray) => {
+    let currentColor = colorArray.color
+    // For each color we were provided…
+    for (let buttonColor of buttonColors) {
+      // …create a button with that color…
+      let button = document.createElement('button')
+      button.dataset.color = buttonColor
+      button.style.backgroundColor = buttonColor
+
+      // …mark the currently selected color…
+      if (buttonColor === currentColor) {
+        button.classList.add(selectedClassName)
+      }
+
+      // …and register a listener for when that button is clicked
+      button.addEventListener('click', handleButtonClick)
+      page.appendChild(button)
+    }
+  })
+}
+
+// Initialize the page by constructing the color options
+constructOptions(presetButtonColors)
